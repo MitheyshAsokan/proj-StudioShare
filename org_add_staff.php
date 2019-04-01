822dc6927e6e9cd262821a3be48c9a44a29a1fef
calvincheungg <48868847+calvincheungg@users.noreply.github.com>
2019-04-01 01:06:14 -0400

    Added dynamic studio name drop down

--- org_add_staff.php
+++ org_add_staff.php
@@ -1,13 +1,31 @@
+<?php
+require 'connect.php';
+require 'org_session.php';
+
+$org_id=$_SESSION['org_id_session'];
+?>
+
 <html>
 <body>
 <h1>Create New Staff Profile</h1>
 <form action="" method="post">
     Staff Name: <input name="staff_name" type="text"><br><br>
-    Username: <input name="username" type="text"><br><br>
-    Position: <input name="position" type="text"><br><br>
+    Username:   <input name="username" type="text"><br><br>
+    Position:   <input name="position" type="text"><br><br>
     Start Date: <input name="start_date" type="date"><br><br>
-    Studio Number: <input name="studio" type="number"><br><br>
-    </select><br><br>
+    Studio:     <select name="studio">
+                    <option value="">Select a studio</option>
+                    <?php
+                        $studio_list = "SELECT * FROM Studio WHERE OrgID ='$org_id'";
+                        $studio_result = mysqli_query($con, $studio_list);
+                        while($row = mysqli_fetch_assoc($studio_result)) {
+                            $studio_name = $row['StudioName'];
+                        ?>
+                        <option value="<?php echo $studio_name?>"><?php echo $studio_name?></option>
+                        <?php
+                        }
+                        ?>
+                </select><br><br>
     <input type="submit" value="Register Staff"><br><br>
     <button type="reset" value="Reset">Clear all fields</button>
 </form><br><br>
@@ -17,10 +35,6 @@
 
 <?php
 
-require 'connect.php';
-require 'org_session.php';
-
-$org_id=$_SESSION['org_id_session'];
 $org_name=$_SESSION['org_name_session'];
 $staff_name=mysqli_real_escape_string($con,$_POST['staff_name']);
 $username=mysqli_real_escape_string($con,$_POST['username']);
@@ -29,6 +43,11 @@ $position=mysqli_real_escape_string($con,$_POST['position']);
 $start_date=mysqli_real_escape_string($con,$_POST['start_date']);
 $studio=mysqli_real_escape_string($con,$_POST['studio']);
 
+$query = "SELECT * FROM Studio WHERE StudioName='$studio'";
+$studio_id_result = mysqli_query($con, $query);
+$row = mysqli_fetch_assoc($studio_id_result);
+$studio_id = $row['StudioID'];
+
 if(empty($staff_name)||empty($username)||empty($position)
     ||empty($start_date)||empty($studio)
     ||trim($staff_name)==''||trim($username)==''
@@ -36,10 +55,11 @@ if(empty($staff_name)||empty($username)||empty($position)
     echo 'You did not fill out the required fields.';
     die();
 }
+
 $query = "INSERT INTO Staff (OrgID, OrganizationName, StaffName, Username,
           Password, Position, StartDate, StudioID) 
           VALUES('$org_id', '$org_name', '$staff_name', '$username', '$password',
-           '$position', '$start_date', '$studio')";
+           '$position', '$start_date', '$studio_id')";
 
 if (mysqli_query($con, $query)) {
     echo "$username added successfully.";
